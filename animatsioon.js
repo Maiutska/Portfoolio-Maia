f06761c71d58d72ac4bd6a8491ce18c70c412c7c
maiamarit <maia-marit.riit@voco.ee>
2026-03-02 15:40:49 +0200

    changes

--- animatsioon.js
+++ animatsioon.js
@@ -4,7 +4,6 @@ const menuHeight = 385; // height of your fixed header
 // Define sections and their desired menu text color
 const sections = [
   { el: document.querySelector("#home"), color: "light" },
-  { el: document.querySelector("#opingud"), color: "dark" },
   { el: document.querySelector("#works"), color: "dark" },
   { el: document.querySelector("#about"), color: "dark" },
   { el: document.querySelector("#contacts"), color: "dark" },
